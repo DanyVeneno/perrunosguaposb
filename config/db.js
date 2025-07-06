8a1aa946116d11228deb5d07f39f8ddbfddcc2c3
DanyVeneno <danieledjv@gmail.com>
2025-07-06 12:36:22 -0600

    first commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import colors from "colors";
+export const db = async () => {
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    //console.log(db.connection);
+    const url = `${db.connection.host}:${db.connection.port}`;
+    console.log(
+      colors.blue(
+        "Pinche putito tu base de datos esta conectada muy chingon",
+        url
+      )
+    );
+  } catch (error) {
+    console.log(`Error:${error.message}`);
+    process.exit(1);
+  }
+};
---
+++ data/beautyServices.js
@@ -0,0 +1,82 @@
+export const services = [
+  {
+    name: "Baño y secado",
+    price: 25.99
+  },
+  {
+    name: "Corte de pelo",
+    price: 35.0
+  },
+  {
+    name: "Paseo individual",
+    price: 15.5
+  },
+  {
+    name: "Guardería por día",
+    price: 45.75
+  },
+  {
+    name: "Entrenamiento básico",
+    price: 50.0
+  },
+  {
+    name: "Masaje relajante",
+    price: 30.99
+  },
+  {
+    name: "Limpieza de oídos",
+    price: 10.5
+  },
+  {
+    name: "Cuidado dental",
+    price: 40.0
+  },
+  {
+    name: "Servicio de veterinario a domicilio",
+    price: 80.0
+  },
+  {
+    name: "Día de spa",
+    price: 65.0
+  },
+  {
+    name: "Peluquería creativa",
+    price: 55.0
+  },
+  {
+    name: "Paseos grupales",
+    price: 12.0
+  },
+  {
+    name: "Hospedaje de lujo",
+    price: 75.0
+  },
+  {
+    name: "Corte de uñas",
+    price: 8.5
+  },
+  {
+    name: "Servicio de recolección y entrega",
+    price: 20.0
+  },
+  {
+    name: "Entrenamiento avanzado",
+    price: 70.0
+  },
+  {
+    name: "Detección de pulgas y garrapatas",
+    price: 12.75
+  },
+  {
+    name: "Catering de comida casera",
+    price: 45.0
+  },
+  {
+    name: "Sesión de fotos profesional",
+    price: 60.0
+  },
+  {
+    name: "Caminatas en la naturaleza",
+    price: 18.5
+  }
+];
